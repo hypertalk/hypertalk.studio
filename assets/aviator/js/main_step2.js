7d9e44dd80c5903ef188984c4550bdedac61ef45
urza <urza@snug.city>
2019-08-14 10:53:44 -0400

    working on tilt

--- assets/aviator/js/main_step2.js
+++ assets/aviator/js/main_step2.js
@@ -595,26 +595,9 @@ function normalize(v,vmin,vmax,tmin, tmax){
 function handleOrientation(event) {
   var x = event.beta;  // In degree in the range [-180,180]
   var y = event.gamma; // In degree in the range [-90,90]
-  // var maxX = document.querySelector('.audio-card').clientWidth  - ball.clientWidth;
-  // var maxY = garden.clientHeight - ball.clientHeight;
-  // output.innerHTML  = "beta : " + x + "\n";
-  // output.innerHTML += "gamma: " + y + "\n";
-
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
-  if (x < -90) { x = -90};
-
-  // To make computation easier we shift the range of 
-  // x and y to [0,180]
-  x += 90;
-  y += 90;
-
-  mousePos.x = (x/180 - 10)
-  mousePos.y = (7/180 - 10)
-
-  // 10 is half the size of the ball
-  // It center the positioning point to the center of the ball
+  mousePos.x = x
+  mousePos.y = y
+  console.log('tilting to ', x,  y)
 }
 
 function init(event){
